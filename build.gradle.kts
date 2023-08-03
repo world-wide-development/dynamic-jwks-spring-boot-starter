a10b8ac0ae86bc0c29ef78d5f360ae4e112a1dc8
serhey-doroshenko-development <serhey.doroshenko.work@gmail.com>
2023-08-03 20:55:10 +0300

    Release 0.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ subprojects {
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
 
-    version = "0.0.1"
+    version = "0.0.2"
     group = "org.development.wide.world.spring"
 
     repositories {
