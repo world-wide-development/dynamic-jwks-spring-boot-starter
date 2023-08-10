183ec1ced76ad87634816f93a032c5dcd4ae7f79
serhey-doroshenko-development <serhey.doroshenko.work@gmail.com>
2023-08-10 17:43:14 +0300

    Add more unit tests

--- vault-dynamic-jwks-spring-boot/src/test/java/org/development/wide/world/spring/vault/jwks/autoconfigure/VaultDynamicJwksAutoConfigurationUnitTest.java
+++ vault-dynamic-jwks-spring-boot/src/test/java/org/development/wide/world/spring/vault/jwks/autoconfigure/VaultDynamicJwksAutoConfigurationUnitTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import core.base.BaseUnitTest;
 import org.development.wide.world.spring.vault.jwks.spi.VaultJwksCertificateRotator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
@@ -11,9 +12,11 @@ import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilde
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.vault.client.RestTemplateCustomizer;
 import org.springframework.vault.core.VaultTemplate;
+import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,6 +36,30 @@ class VaultDynamicJwksAutoConfigurationUnitTest extends BaseUnitTest {
             assertThat(context).hasSingleBean(VaultJwksCertificateRotator.class);
             assertThat(context).hasSingleBean(RestTemplateCustomizer.class);
             assertThat(context).hasSingleBean(JWKSource.class);
+        });
+    }
+
+    @Test
+    void testRestTemplateCustomizer() {
+        final ApplicationContextRunner applicationContextRunner = this.contextRunner
+                .withPropertyValues("vault-dynamic-jwks.enabled=on")
+                .withUserConfiguration(UnitTestUserConfiguration.class);
+        applicationContextRunner.run(context -> {
+            final RestTemplate restTemplate = new RestTemplate();
+            final var customizer = context.getBean(RestTemplateCustomizer.class);
+            Assertions.assertDoesNotThrow(() -> customizer.customize(restTemplate));
+        });
+    }
+
+    @Test
+    void testJackson2ObjectMapperBuilderCustomizer() {
+        final ApplicationContextRunner applicationContextRunner = this.contextRunner
+                .withPropertyValues("vault-dynamic-jwks.enabled=on")
+                .withUserConfiguration(UnitTestUserConfiguration.class);
+        applicationContextRunner.run(context -> {
+            final Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder = Jackson2ObjectMapperBuilder.json();
+            final var customizer = context.getBean(Jackson2ObjectMapperBuilderCustomizer.class);
+            Assertions.assertDoesNotThrow(() -> customizer.customize(jacksonObjectMapperBuilder));
         });
     }
 
