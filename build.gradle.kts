f3ec9e57aaae23dd0fd41cc154c4f539c446eb7c
serhey-doroshenko-development <serhey.doroshenko.work@gmail.com>
2023-08-31 11:28:08 +0300

    Release 0.0.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ subprojects {
     apply(plugin = "org.owasp.dependencycheck")
     apply(plugin = "io.spring.dependency-management")
 
-    version = "0.0.5"
+    version = "0.0.6"
     group = "io.github.world-wide-development"
 
     repositories {
