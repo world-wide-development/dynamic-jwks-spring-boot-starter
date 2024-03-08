cecff048f6a95a48426eb4e04a9a8d553baecbc5
serhey-doroshenko-development <serhey.doroshenko.work@gmail.com>
2024-03-08 20:01:12 +0200

    Update Gradle and Java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,7 +82,7 @@ allprojects {
 
     java {
         toolchain {
-            languageVersion.set(JavaLanguageVersion.of(17))
+            languageVersion.set(JavaLanguageVersion.of(21))
         }
     }
 
