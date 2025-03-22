c70ecda1d6deac719395c05410688980d3d08002
serhey-doroshenko-development <serhey.doroshenko.work@gmail.com>
2025-03-22 21:59:14 +0200

    Update samples

--- samples/dynamic-vault-jwks-authorization-server/build.gradle.kts
+++ samples/dynamic-vault-jwks-authorization-server/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("org.springframework.boot") version "3.4.1"
+    id("org.springframework.boot") version "3.4.4"
 }
 
-val springCloudVersion = "2024.0.0"
+val springCloudVersion = "2024.0.1"
 
 tasks.getByName("dependencyCheckAnalyze") {
     enabled = false
