987693c7de00fbe3868117f272c4899349b72c34
serhey-doroshenko-development <serhey.doroshenko.work@gmail.com>
2025-08-26 00:22:23 +0300

    Rename env var for "dynamic jwks" less release

--- dynamic-jwks/build.gradle.kts
+++ dynamic-jwks/build.gradle.kts
@@ -1,10 +1,8 @@
 @file:Suppress("unused", "UnstableApiUsage")
 
-import org.jreleaser.model.Active
-
 plugins {
-    id("org.jreleaser")
-    id("maven-publish")
+//    id("org.jreleaser")
+//    id("maven-publish")
 }
 
 java {
@@ -53,88 +51,88 @@ testing {
     }
 }
 
-jreleaser {
-    gitRootSearch.set(true)
-    release {
-        github {
-            sign.set(false)
-            skipTag.set(true)
-            token.set("no-op")
-            skipRelease.set(true)
-        }
-    }
-    signing {
-        armored.set(true)
-        active.set(Active.ALWAYS)
-        publicKey.set(System.getenv("MAVEN_GPG_PUBLIC_KEY"))
-        secretKey.set(System.getenv("MAVEN_GPG_PRIVATE_KEY"))
-        passphrase.set(System.getenv("MAVEN_GPG_PASSPHRASE"))
-    }
-    deploy {
-        maven {
-            mavenCentral {
-                register("sonatype") {
-                    active.set(Active.ALWAYS)
-                    stagingRepository("build/staging-deploy")
-                    username.set(System.getenv("MAVEN_USERNAME"))
-                    password.set(System.getenv("MAVEN_PASSWORD"))
-                    url.set("https://central.sonatype.com/api/v1/publisher")
-                }
-            }
-        }
-    }
-}
-
-publishing {
-    repositories {
-        maven {
-            setUrl(layout.buildDirectory.dir("staging-deploy"))
-        }
-    }
-    publications {
-        register<MavenPublication>("dynamic-jwks") {
-            from(components["java"])
-            versionMapping {
-                usage("java-runtime") {
-                    fromResolutionResult()
-                }
-                usage("java-api") {
-                    fromResolutionOf("runtimeClasspath")
-                }
-            }
-            pom {
-                name.set("Dynamic JWKS")
-                inceptionYear.set("2023")
-                developers {
-                    developer {
-                        id.set("serhey")
-                        timezone.set("Europe/Kyiv")
-                        name.set("Serhey Doroshenko")
-                        organization.set("World Wide Development")
-                        email.set("serhey.doroshenko.work@gmail.com")
-                    }
-                }
-                organization {
-                    name.set("World Wide Development")
-                    url.set("https://github.com/world-wide-development")
-                }
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                url.set("https://github.com/world-wide-development/dynamic-jwks-spring-boot-starter")
-                description.set("Dynamic JWKS Spring Boot Starter developed by World Wide Development")
-                scm {
-                    tag.set("dynamic-jwks")
-                    url.set("https://github.com/world-wide-development/dynamic-jwks-spring-boot-starter")
-                    // @formatter:off
-                    connection.set("scm:git:git://github.com:world-wide-development/dynamic-jwks-spring-boot-starter.git")
-                    developerConnection.set("scm:git:ssh://git@github.com:world-wide-development/dynamic-jwks-spring-boot-starter.git")
-                    // @formatter:on
-                }
-            }
-        }
-    }
-}
+//jreleaser {
+//    gitRootSearch.set(true)
+//    release {
+//        github {
+//            sign.set(false)
+//            skipTag.set(true)
+//            token.set("no-op")
+//            skipRelease.set(true)
+//        }
+//    }
+//    signing {
+//        armored.set(true)
+//        active.set(Active.ALWAYS)
+//        publicKey.set(System.getenv("MAVEN_GPG_PUBLIC_KEY"))
+//        secretKey.set(System.getenv("MAVEN_GPG_PRIVATE_KEY"))
+//        passphrase.set(System.getenv("MAVEN_GPG_PASSPHRASE"))
+//    }
+//    deploy {
+//        maven {
+//            mavenCentral {
+//                register("sonatype") {
+//                    active.set(Active.ALWAYS)
+//                    stagingRepository("build/staging-deploy")
+//                    username.set(System.getenv("MAVEN_USERNAME"))
+//                    password.set(System.getenv("MAVEN_PASSWORD"))
+//                    url.set("https://central.sonatype.com/api/v1/publisher")
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//publishing {
+//    repositories {
+//        maven {
+//            setUrl(layout.buildDirectory.dir("staging-deploy"))
+//        }
+//    }
+//    publications {
+//        register<MavenPublication>("dynamic-jwks") {
+//            from(components["java"])
+//            versionMapping {
+//                usage("java-runtime") {
+//                    fromResolutionResult()
+//                }
+//                usage("java-api") {
+//                    fromResolutionOf("runtimeClasspath")
+//                }
+//            }
+//            pom {
+//                name.set("Dynamic JWKS")
+//                inceptionYear.set("2023")
+//                developers {
+//                    developer {
+//                        id.set("serhey")
+//                        timezone.set("Europe/Kyiv")
+//                        name.set("Serhey Doroshenko")
+//                        organization.set("World Wide Development")
+//                        email.set("serhey.doroshenko.work@gmail.com")
+//                    }
+//                }
+//                organization {
+//                    name.set("World Wide Development")
+//                    url.set("https://github.com/world-wide-development")
+//                }
+//                licenses {
+//                    license {
+//                        name.set("The Apache License, Version 2.0")
+//                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+//                    }
+//                }
+//                url.set("https://github.com/world-wide-development/dynamic-jwks-spring-boot-starter")
+//                description.set("Dynamic JWKS Spring Boot Starter developed by World Wide Development")
+//                scm {
+//                    tag.set("dynamic-jwks")
+//                    url.set("https://github.com/world-wide-development/dynamic-jwks-spring-boot-starter")
+//                    // @formatter:off
+//                    connection.set("scm:git:git://github.com:world-wide-development/dynamic-jwks-spring-boot-starter.git")
+//                    developerConnection.set("scm:git:ssh://git@github.com:world-wide-development/dynamic-jwks-spring-boot-starter.git")
+//                    // @formatter:on
+//                }
+//            }
+//        }
+//    }
+//}
